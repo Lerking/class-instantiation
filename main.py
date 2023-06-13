c61b18ec939a55ffde517ad4ee898616c97e8c7c
Jan Lerking <jan.lerking@alfalaval.com>
2023-06-13 07:27:52 +0200

    updated for testing. /JL

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random, string
 
 class Person:
     def __init__(self, name :str, age :int):
@@ -5,7 +6,7 @@ class Person:
         self.age = age
 
     def say_hello(self):
-        print(f"Hello, I'm {self.name}")
+        print(f"Hello, I'm {self.name} and I'm {self.age} years old")
 
 class Group:
     def __init__(self, name :str):
@@ -14,7 +15,7 @@ class Group:
 
     def print_members(self):
         for member in self.members:
-            print(f"Hello, I'm {member.name}")
+            print(f"Hello, I'm {member.name} and I'm {member.age} years old")
             
     def add_member(self, member: Person):
         self.members.append(member)
@@ -25,18 +26,22 @@ class Group:
         return self.members[-1]
             
 if __name__ == '__main__':
-    person = Person("John", 36)
-    person.say_hello()
+    #person = Person("John", 36)
+    #person.say_hello()
     
     Group = Group("Group 1")
-    Group.members.append(Person("John", 36))
-    Group.members.append(Person("Mary", 24))
+    #Group.members.append(Person("John", 36))
+    #Group.members.append(Person("Mary", 24))
+    #Group.print_members()
+    #Group.add_member(Person("Peter", 48))
+    #Group.print_members()
+    #Group.add_member(Person("Benjamin", 28))
+    #Group.print_members()
+    for _ in range(100):
+        Group.create_person_member(
+            ''.join(random.choices(string.ascii_lowercase, k=5)),
+            random.randint(1,100)
+            )
     Group.print_members()
-    Group.add_member(Person("Peter", 48))
-    Group.print_members()
-    Group.add_member(Person("Benjamin", 28))
-    Group.print_members()
-    test = Group.create_person_member("Julia", 32)
-    Group.print_members()
-    print(test)
+    #print(test)
     
